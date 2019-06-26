c5d04a984e88fca819163a513a99820ec1efe54b
eric <Eric355402@yahoo.com>
2019-06-26 12:15:40 -0600

    imported headers into HeaderContent and imported that into app.js

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Header.css';
+import HeaderTitle from './HeaderTitle';
+import ImageThumbnail from './ImageThumbnail';
 
 const HeaderContent = () => (
   <div>
     <HeaderTitle />
     This is the Header Content
+    <ImageThumbnail />
+    Image Above
   </div>
 );
 
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = () => (
+  <div>
+    <img src="Project-I-Social-Card\design-files\social-card.png" alt="" />
+  </div>
+);
+export default ImageThumbnail;
